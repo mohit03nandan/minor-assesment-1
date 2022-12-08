a7f9a02605d3f9c77b3941ea38c5b1f24643bef3
Mohit <mohitnandan81825@gmail.com>
2022-12-09 00:28:17 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express')
+const mongoose = require("mongoose")
+const dotenv = require('dotenv');
 const app = express()
 
+//configuration mongodb with localserver
+const mongoUrl = "mongodb://localhost:27017/micro-project"
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database Connected Successfully");
+    })
+    .catch((err) => {
+        console.log("Database Not Connected Successfully : " + err);
+    });
+
+
+
+//check server is active or not
+app.get("/api/health" ,(req,res) =>{
+    res.send(`backend server is active status: active & time:${ new Date()}`)
+})
 
 
 app.get('/', (req, res) => {
