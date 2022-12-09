f23902fd34427dc824e162e507368305e92a98be
Mohit <mohitnandan81825@gmail.com>
2022-12-09 17:43:52 +0530

    added databases config and initialzation

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
 const express = require('express')
-const mongoose = require("mongoose")
-const dotenv = require('dotenv');
-const app = express()
-
-//configuration mongodb with localserver
-const mongoUrl = "mongodb://localhost:27017/micro-project"
-mongoose.set('strictQuery', false);
-mongoose
-    .connect(mongoUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("Database Connected Successfully");
-    })
-    .catch((err) => {
-        console.log("Database Not Connected Successfully : " + err);
-    });
-
+const mongoose = require("./config/db")
+const app = express();
 
 
 //check server is active or not
@@ -33,8 +16,12 @@ app.get('/', (req, res) => {
 
 
 //connection part
-const port = process.env.PORT || 3000
-const host = process.env.HOST || "localhost"
+const port = process.env.PORT 
+const host = process.env.HOST 
 app.listen(port, () => {
     console.log(`Express server listening at http://${host}:${port}`)
 })
+
+
+
+
