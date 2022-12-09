28ec9428bb1de570517f244261daa429398cf1e4
mohit03nandan <mohitnandan81825@gmail.com>
2022-12-09 19:01:42 +0530

    added databases model

--- server.js
+++ server.js
@@ -2,17 +2,41 @@ const express = require('express')
 const mongoose = require("./config/db")
 const app = express();
 
+const gallerySchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    createdAt: Date,
+    updatedAt: Date
+});
+
+const imageSchema = new mongoose.Schema({
+    name: String,
+    _id: Number,
+    createdAt: Date,
+    updatedAt: Date,
+    category: [String],
+    imageLink: String,
+    likes: Boolean
+});
+
+const gallary = mongoose.model('gallary', gallerySchema);
+const image = mongoose.model('image', imageSchema);
+
+
+
+
+
+
+
 
 //check server is active or not
 app.get("/api/health" ,(req,res) =>{
     res.send(`backend server is active status: active & time:${ new Date()}`)
 })
 
-
 app.get('/', (req, res) => {
     res.send("hello world")
 })
-
 
 
 //connection part
