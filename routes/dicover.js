e5b84491cc93a210335b1e93eda30c49304286d4
mohit03nandan <mohitnandan81825@gmail.com>
2022-12-10 22:42:13 +0530

    added discover images api

--- routes/dicover.js
+++ routes/dicover.js
@@ -5,16 +5,32 @@ const schema = require("../models/schema");
 const route = Router()
 
 var gallarie = schema.gallary;
+var images = schema.image;
 
-route.get("/", async (req,res)=>{
-   
-  const result =  await gallarie.find();
-  console.log(result);
-  res.send(result);
-  res.send("you are now enter in discover route")
+route.get("/", async (req,res,next)=>{
+       try{
+         const result =  await gallarie.find();
+         console.log(result);
+         res.send(result);
+         res.send("you are now enter in discover route")
+       }
+       catch(error){
+         next();
+       }
 
 })
 
-
+route.get("/:category", async (req,res,next)=>{
+          try{
+           const categoryImage = req.params.category;
+           const result =  await images.find({category: categoryImage}).limit(4);
+           res.send(result);
+          }
+          catch(error){
+           next();
+          } 
+  })
+  
+  
 
 module.exports = route;
