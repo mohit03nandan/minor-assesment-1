43e9f1524dab60f04d696e97a87ed4acb2b239df
mohit03nandan <mohitnandan81825@gmail.com>
2022-12-11 10:47:53 +0530

    added filters to discover api

--- routes/dicover.js
+++ routes/dicover.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require('express')
 const {Router} = require("express")
 const schema = require("../models/schema");
+const { ObjectID } = require("bson");
 const route = Router()
 
 var gallarie = schema.gallary;
@@ -20,17 +21,52 @@ route.get("/", async (req,res,next)=>{
 
 })
 
+//filter by likes
+route.get("/like", async(req,res,next)=>{
+  try{
+    const result =  await images.find({likes : 1});    
+    res.send(result)
+  } 
+  catch(error){
+    next()
+  }    
+
+})
+
+//sort by date in ascending or descending order
 route.get("/:category", async (req,res,next)=>{
-          try{
-           const categoryImage = req.params.category;
-           const result =  await images.find({category: categoryImage}).limit(4);
-           res.send(result);
-          }
-          catch(error){
-           next();
-          } 
+      const categoryImage = req.params.category;
+      try{   
+             
+             if(req.query.sortBy){ 
+                const str = req.query.sortBy.split(':')  
+
+                if(str[1] === 'desc'){
+                  //?sortBy=createdBy:desc
+                  const result =  await images.find({category: categoryImage}).limit(4).sort({createdAt:-1});
+                  res.send(result);
+                }
+
+                else if(str[1] === 'asc'){
+                  //?sortBy=createdBy:asc
+                  const result =  await images.find({category: categoryImage}).limit(4).sort({createdAt:1});
+                  res.send(result);
+                }
+
+            }  
+
+            else{
+                const result =  await images.find({category: categoryImage}).limit(4);
+                res.send(result); 
+            }
+
+    }catch(error){
+        next();
+       }      
+
   })
-  
+
+
   
 
 module.exports = route;
