539a848a3b0359095a874c5959e759acaf09189e
mohit03nandan <mohitnandan81825@gmail.com>
2022-12-12 21:05:58 +0530

    reset schema after taking refrence from github

--- models/schema.js
+++ models/schema.js
@@ -3,35 +3,46 @@ const mongoose = require("mongoose")
 
 
     const gallerySchema = new mongoose.Schema({ 
-        name:String,
-        createdAt: Date,
-        updatedAt: Date
+      
+            name: {
+                type: String,
+                required: true,
+            },
+
+            createdAt: Date,
+            updatedAt: Date
+            
     });
+
     
     const imageSchema = new mongoose.Schema({
-        name: String,
+        name: {
+            type: String,
+            required: true,
+        },
+        category: { 
+            type: Array, 
+            required: true
+         },
+        likes: { 
+            type: Number,
+             default: 0 
+            },
+        imageLink: { 
+            type: String, 
+            required: true 
+        },
         createdAt: Date,
-        updatedAt: Date,
-        category: [String],
-        imageLink: String,
-        likes: Number,
+        updatedAt: Date
        
     });
     
-    const favouriteSchema = new mongoose.Schema({ 
-        name:String,
-        createdAt: Date,
-        updatedAt: Date
-    });
-    
-
     const gallary = mongoose.model('gallary', gallerySchema);
     const image = mongoose.model('image', imageSchema);
-    const favourite = mongoose.model('favourite', favouriteSchema)
     
     var my_schemas = {
        "gallary": gallary,
        "image": image,
-       "favourite": favourite
     };
+
 module.exports = my_schemas;
