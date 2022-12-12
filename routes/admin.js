b778b20046a4975907ef68bdd27027a12e6021de
mohit03nandan <mohitnandan81825@gmail.com>
2022-12-12 21:46:49 +0530

    reset admin after taking refrence from github

--- routes/admin.js
+++ routes/admin.js
@@ -6,55 +6,64 @@ const route = Router()
 
 var gallaries = schema.gallary;
 var images = schema.image;
-var fav = schema.favourite;
 
-route.post("/:category", (req,res,next)=>{
+route.post("/:category", async (req,res,next)=>{
      
     try{
-      const imageName = req.body.name;
-      const imageLink = req.body.imageLink;
-      const category = req.params.category;
-
-      const newImage = new images({
-      name: imageName,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      category: category,
-      imageLink: imageLink,
-      likes: 0
-     })
-     
-//add favourite image in seperate coolection favourite       
-      if(req.query.favourite){
-        var str = req.query.favourite.split()
-        if(str[0] === 'true'){
-          const favourites = new fav({
-             name: imageName,
-             createdAt: new Date(),
-             updatedAt: new Date(),
-          })
-          favourites.save();
-        } 
-      }
-      
-
       const categoryName = req.params.category;
+
+      if (!category) {
+        res.status(400).send("Bad Request");
+       }
+
       const gallaryCollection = new gallaries({
          name: categoryName,
          createdAt: new Date(),
          updatedAt: new Date(),
       })
-
-       res.send("you are inside admin")
-       newImage.save();
-       gallaryCollection.save();
-      
+        
+       await gallaryCollection.save();
+       res.send("Category created successfully!")
+       
 
        }catch(error) {
          next();
        }
     
 })
+
+
+route.post("/add-image", async (req, res, next) => {
+try{
+
+
+      const imageName = req.body.name;
+      const imageLink = req.body.imageLink;
+      const category = req.body.category;
+
+     if (!imageName || !category.length || !imageLink) {
+      res.status(400).send("Bad Request");
+     }
+
+       
+     const newImage = new images({
+      name: imageName,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      category: category,
+      imageLink: imageLink,
+      
+     })
+     
+ 
+
+   newImage.save();
+   res.send("Image added successfully!");
+  }catch(error){
+    next();
+   }
+  
+});
 
 
 module.exports = route;
